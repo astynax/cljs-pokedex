f00ba0616cb79a28f983c9d17f00efb3ae0470aa
aleksei.pirogov <aleksei.pirogov@jetbrains.com>
2023-03-21 20:25:22 +0400

    update the backend with some demos

--- project.clj
+++ project.clj
@@ -7,6 +7,7 @@
                  [ring/ring-core "1.9.6"]
                  [ring/ring-devel "1.9.6"]
                  [ring/ring-jetty-adapter "1.9.6"]
+                 [rum "0.12.10" :exclusions [cljsjs/react cljsjs/react-dom]]
                  [compojure "1.7.0"]]
   :main ^:skip-aot clj-pokedex.core
   :target-path "target/%s"
--- src/clj_pokedex/core.clj
+++ src/clj_pokedex/core.clj
@@ -2,12 +2,44 @@
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
             [ring.adapter.jetty :refer [run-jetty]]
-            [ring.middleware.reload :refer [wrap-reload]])
+            [ring.middleware.reload :refer [wrap-reload]]
+            [ring.middleware.params :refer [wrap-params]]
+            [ring.middleware.keyword-params :refer [wrap-keyword-params]]
+            [ring.util.response :refer [redirect]]
+            [rum.core :as rum])
   (:gen-class))
 
+(defonce state (atom 0))
+
 (defroutes app
-  (GET "/" [] "<h1>Hello World</h1>")
-  (route/not-found "<h1>Page not found</h1>"))
+  (GET "/" []
+    (rum/render-html
+     [:div
+      [:h1 "Counter"]
+      (str @state)
+      [:a {:href "/inc" :style {:border "1px solid red;"}} "+"]
+      [:a {:href "/dec" :style {:border "1px solid red;"}} "-"]]))
+
+  (GET "/inc" []
+    (swap! state inc)
+    (redirect "/"))
+
+  (GET "/dec" []
+    (swap! state dec)
+    (redirect "/"))
+
+  (GET "/greet" []
+    (rum/render-html
+     [:form {:method :POST}
+      [:label "Name"
+       [:input {:name "name"}]]
+      [:button {:type "submit"} "Greet!"]]))
+
+  (POST "/greet" {{n :name} :params}
+    (rum/render-html
+     [:h1 "Hello, " n "!"]))
+
+  (route/not-found "<h1>Page not found :(</h1>"))
 
 (defn -main
   [& args]
@@ -15,3 +47,22 @@
   (-> #'app
       wrap-reload
       (run-jetty {:port 4000})))
+
+;; REPL-related stuff
+
+(defonce server (atom nil))
+
+(defn- start []
+  (when-not @server
+    (reset! server
+            (-> #'app
+                wrap-keyword-params
+                wrap-params
+                (run-jetty {:port 4000 :join? false})))))
+
+(defn- stop []
+  (when @server
+    (.stop @server)
+    (reset! server nil)))
+
+(defn- restart [] (stop) (start))
